bb915d939345e602d7bae02c544c13f4623d699a
DevExpressExampleBot <fakeEmail>
2021-09-07 00:55:18 +0300

    Source auto update [skip ci]

--- VB/ViewModel/ChildViewModel.vb
+++ VB/ViewModel/ChildViewModel.vb
@@ -2,15 +2,15 @@
 Imports DevExpress.Mvvm.DataAnnotations
 
 Namespace Example.ViewModel
-    Public Class ChildViewModel
-        <ServiceProperty(SearchMode:=ServiceSearchMode.PreferParents)> _
-        Protected Overridable ReadOnly Property MessageBoxService() As IMessageBoxService
-            Get
-                Return Nothing
-            End Get
-        End Property
-        Public Sub ShowMessage()
-            MessageBoxService.Show("This is ChildView")
-        End Sub
-    End Class
+	Public Class ChildViewModel
+		<ServiceProperty(SearchMode:=ServiceSearchMode.PreferParents)>
+		Protected Overridable ReadOnly Property MessageBoxService() As IMessageBoxService
+			Get
+				Return Nothing
+			End Get
+		End Property
+		Public Sub ShowMessage()
+			MessageBoxService.Show("This is ChildView")
+		End Sub
+	End Class
 End Namespace
--- VB/ViewModel/MainViewModel.vb
+++ VB/ViewModel/MainViewModel.vb
@@ -2,14 +2,14 @@
 Imports System.Windows.Input
 
 Namespace Example.ViewModel
-    Public Class MainViewModel
-        Protected Overridable ReadOnly Property MessageBoxService() As IMessageBoxService
-            Get
-                Return Nothing
-            End Get
-        End Property
-        Public Sub ShowMessage()
-            MessageBoxService.Show("This is MainView!")
-        End Sub
-    End Class
+	Public Class MainViewModel
+		Protected Overridable ReadOnly Property MessageBoxService() As IMessageBoxService
+			Get
+				Return Nothing
+			End Get
+		End Property
+		Public Sub ShowMessage()
+			MessageBoxService.Show("This is MainView!")
+		End Sub
+	End Class
 End Namespace
