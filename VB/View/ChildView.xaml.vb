2f33bb9f7dd541a88d1269666277a80cb70b6cb1
DevExpressExampleBot <fakeEmail>
2022-12-17 15:05:59 +0400

    Source auto update [skip ci]

--- VB/View/ChildView.xaml.vb
+++ VB/View/ChildView.xaml.vb
@@ -1,11 +1,12 @@
-﻿Imports System.Windows.Controls
+Imports System.Windows.Controls
 
 Namespace Example.View
-	Partial Public Class ChildView
-		Inherits UserControl
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
-	End Class
+    Public Partial Class ChildView
+        Inherits UserControl
+
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+    End Class
 End Namespace
--- VB/ViewModel/MainViewModel.vb
+++ VB/ViewModel/MainViewModel.vb
@@ -1,15 +1,17 @@
-﻿Imports DevExpress.Mvvm
-Imports System.Windows.Input
+Imports DevExpress.Mvvm
 
 Namespace Example.ViewModel
-	Public Class MainViewModel
-		Protected Overridable ReadOnly Property MessageBoxService() As IMessageBoxService
-			Get
-				Return Nothing
-			End Get
-		End Property
-		Public Sub ShowMessage()
-			MessageBoxService.Show("This is MainView!")
-		End Sub
-	End Class
+
+    Public Class MainViewModel
+
+        Protected Overridable ReadOnly Property MessageBoxService As IMessageBoxService
+            Get
+                Return Nothing
+            End Get
+        End Property
+
+        Public Sub ShowMessage()
+            MessageBoxService.Show("This is MainView!")
+        End Sub
+    End Class
 End Namespace
